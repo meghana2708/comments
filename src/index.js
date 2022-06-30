bbd49e72c9cd065cf33f55f5d9ffe77f149eed21
Sai Meghana Vemula <meghana.bamr@gmail.com>
2022-06-30 17:09:30 +0530

    Aproval

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css'
+import CommentDetail from './commentDetail';
+import ApprovalCard from './ApprovalCard';
+import { faker } from '@faker-js/faker';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a functions
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+const root =ReactDOM.createRoot(document.querySelector('#root'))
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+
+      <ApprovalCard>
+        <CommentDetail
+          author = "Sam"
+          timeAgo = "Today at 4:45 PM"
+          avatar = {faker.image.avatar()}
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+      <CommentDetail
+          author = "Jane"
+          timeAgo = "Today at 4:45 PM"
+          avatar = {faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+      <CommentDetail
+          author = "Sam"
+          timeAgo = "Today at 4:45 PM"
+          avatar = {faker.image.avatar()}
+        /></ApprovalCard> 
+    </div>
+    
+  );
+  
+};
+root.render(App());
+// const root =ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(App)
